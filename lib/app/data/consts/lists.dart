2a3ac6010d92df33b537998de46c23185f6f4312
Softdrinks14 <hisyambima5@gmail.com>
2024-11-09 19:56:26 +0700

    HOME

---
+++ lib/app/data/consts/lists.dart
@@ -0,0 +1,20 @@
+import 'package:aplikasi_booking_dokter/app/data/consts/images.dart';
+import 'package:aplikasi_booking_dokter/app/data/consts/strings.dart';
+
+var iconsList = [
+  AppAssets.icBody,
+  AppAssets.icEar,
+  AppAssets.icLiver,
+  AppAssets.icLungs,
+  AppAssets.icHeart,
+  AppAssets.icKidney
+];
+
+var iconsTitleList = [
+  AppStrings.body,
+  AppStrings.ear,
+  AppStrings.liver,
+  AppStrings.lungs,
+  AppStrings.heart,
+  AppStrings.kidney,
+];
